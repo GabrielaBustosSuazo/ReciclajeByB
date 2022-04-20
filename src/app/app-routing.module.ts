b7bdff32006765f7d6e25f8b8c211aed5c5de298
NeutronCL <jbecofla@gmail.com>
2022-04-20 13:18:07 -0400

    carpetas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,11 +10,31 @@ const routes: Routes = [
 
   {
     path: 'valoracion',
-    loadChildren: () => import('./valoracion/valoracion.module').then( m => m.ValoracionPageModule)
+    loadChildren: () => import('./pages/valoracion/valoracion.module').then( m => m.ValoracionPageModule)
   },
   {
     path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'ingreso-evidencias',
+    loadChildren: () => import('./pages/ingreso-evidencias/ingreso-evidencias.module').then( m => m.IngresoEvidenciasPageModule)
+  },
+  {
+    path: 'inicio-administrador',
+    loadChildren: () => import('./pages/inicio-administrador/inicio-administrador.module').then( m => m.InicioAdministradorPageModule)
+  },
+  {
+    path: 'inicio-cliente',
+    loadChildren: () => import('./pages/inicio-cliente/inicio-cliente.module').then( m => m.InicioClientePageModule)
+  },
+  {
+    path: 'inicio-recolector',
+    loadChildren: () => import('./pages/inicio-recolector/inicio-recolector.module').then( m => m.InicioRecolectorPageModule)
+  },
+  {
+    path: 'notificaciones',
+    loadChildren: () => import('./pages/notificaciones/notificaciones.module').then( m => m.NotificacionesPageModule)
   },
   {
      path: 'folder/:id',
