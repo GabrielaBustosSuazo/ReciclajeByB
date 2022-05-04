6c2c1c23e306bde11cc36350210ddd3fd265f732
GabrielaBustosSuazo <ga.bustoss@duocuc.cl>
2022-05-04 18:07:32 -0400

    "toolbar actualizado"

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/compat/firestore';
-import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
