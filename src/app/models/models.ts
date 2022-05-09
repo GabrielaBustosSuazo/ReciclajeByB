bcf26606ee5f79599327d0bc722abf4c38ed0203
NeutronCL <jbecofla@gmail.com>
2022-05-09 19:51:58 -0400

    crud cliente(solo agregar) + diseño nuevo

--- src/app/models/models.ts
+++ src/app/models/models.ts
@@ -1,8 +1,19 @@
 export interface Camiones {
-        patente: string;
-        marca: string;
-        modelo: string;
-        anno: string;
-        id: string;
-    }
+  patente: string;
+  marca: string;
+  modelo: string;
+  anno: string;
+  id: string;
+}
 
+export interface Cliente {
+  run: string;
+  nombre: string;
+  direccion: string;
+  telefono: string;
+  usuario: string;
+  contrasena: string;
+  tipoplan: any;
+  rol: string;
+  id: string;
+}
