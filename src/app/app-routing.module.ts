1894a881af4ee918ee6599e529dca4c998c2e67f
GabrielaBustosSuazo <ga.bustoss@duocuc.cl>
2022-05-24 20:18:26 -0400

    "validaciones formularios modificar"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'gestion-cliente',
+    redirectTo: 'gestion-recolector',
     pathMatch: 'full',
   },
 
--- src/app/pages/listar-recolectores/listar-recolectores.page.ts
+++ src/app/pages/listar-recolectores/listar-recolectores.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Camiones, Recolectores } from 'src/app/models/models';
 import { FirestoreService } from 'src/app/services/firestore.service';
 import { UserInteractionService } from 'src/app/services/user-interaction.service';
@@ -13,9 +14,58 @@ export class ListarRecolectoresPage implements OnInit {
   data: Recolectores[] = [];
   camiones: Camiones[] = [];
 
+  get run() {
+    return this.recolectoresForm.get('run');
+  }
+  get nombre() {
+    return this.recolectoresForm.get('nombre');
+  }
+  get direccion() {
+    return this.recolectoresForm.get('direccion');
+  }
+  get telefono() {
+    return this.recolectoresForm.get('telefono');
+  }
+  get camionDesignado() {
+    return this.recolectoresForm.get('camionDesignado');
+  }
+
+  public errorMessages = {
+    run: [
+      { type: 'required', message: 'Run no puede estar vacío' },
+      { type: 'pattern', message: 'Ingrese formato correcto: XX.XXX.XXX-X' },
+    ],
+    nombre: [{ type: 'required', message: 'Nombre no puede estar vacío' }],
+    direccion: [
+      { type: 'required', message: 'Dirección no puede estar vacío' },
+    ],
+    telefono: [
+      { type: 'required', message: 'Teléfono no puede estar vacío' },
+      { type: 'minlength', message: 'Teléfono debe tener 8 numeros' },
+    ],
+    camionDesignado: [
+      { type: 'required', message: 'Debes seleccionar un camion' },
+    ],
+  };
+
+  recolectoresForm = this.formBuilder.group({
+    run: [
+      '',
+      [
+        Validators.required,
+        Validators.pattern(/([0-9]{2}.[0-9].{3}.[0-9].-[0-9(k)(K)])/),
+      ],
+    ],
+    nombre: ['', [Validators.required]],
+    direccion: ['', [Validators.required]],
+    telefono: ['', [Validators.required, Validators.minLength(8)]],
+    camionDesignado: ['', [Validators.required]],
+  });
+
   constructor(
     private database: FirestoreService,
-    private userInteraction: UserInteractionService
+    private userInteraction: UserInteractionService,
+    private formBuilder: FormBuilder
   ) {}
 
   ngOnInit() {
