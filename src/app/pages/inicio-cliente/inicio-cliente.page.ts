2241e46f4efa7ee6acafe13e7db44b8de018368a
GabrielaBustosSuazo <ga.bustoss@duocuc.cl>
2022-05-25 21:17:47 -0400

    interpolacion nombre de usuario logeado

--- src/app/pages/inicio-cliente/inicio-cliente.page.ts
+++ src/app/pages/inicio-cliente/inicio-cliente.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { Usuario } from 'src/app/models/models';
+import { FirestoreService } from 'src/app/services/firestore.service';
 import { FirestoreauthService } from 'src/app/services/firestoreauth.service';
 import { UserInteractionService } from 'src/app/services/user-interaction.service';
 
@@ -10,27 +13,65 @@ import { UserInteractionService } from 'src/app/services/user-interaction.servic
   styleUrls: ['./inicio-cliente.page.scss'],
 })
 export class InicioClientePage implements OnInit {
-
+ nombreUsuario = ''
   constructor(private router:Router, 
               public auth: FirestoreauthService,
-              public userinterface: UserInteractionService) { 
+              public userinterface: UserInteractionService,
+              public alertController: AlertController,
+              public firestore: FirestoreService) { 
+                this.auth.stateUser().subscribe( resp => {
+                  if(resp){
+                    this.getUserInfo(resp.uid)
+                    console.log('esta logeado')
+                  }
+                  else{
+                    console.log('no esta logeado')
+                  }
+              })
 
               }
 
 ngOnInit() {
+  
   }
 
   gotoNotifications() {
     this.router.navigate(['/notificaciones']);
   }
 
-  logout(){
-    this.userinterface.presentToast("Cerrando sesión...")
-    setTimeout(function () {
-      location.reload();
-    }, 0);
-    this.auth.logout();
-    this.userinterface.stop()
-    this.router.navigate(['/login'])
+  async logout() {
+    const alert = await this.alertController.create({
+      header: 'Salir',
+      message: '¿Deseas cerrar sesión?',
+      buttons: [
+          {
+            text: 'Denegar',
+            handler: (blah) => {
+              console.log('Confirma Permiso Denegado: yes');
+            }
+          }, {
+            text: 'Permitir',
+            handler: () => {
+              setTimeout(function () {
+                location.reload();
+              }, 100);
+              this.auth.logout();
+              this.userinterface.presentToast("Cerrando sesión...")
+              this.router.navigate(['/login'])
+              console.log('Confirma Permiso Permitido: yes');
+            }
+          }
+        ]
+      });
+      await alert.present();
   }
+  getUserInfo(uid: string){
+    const path = 'Usuarios'
+        const id = uid;
+        this.firestore.getUserInfo<Usuario>(path, id).subscribe(respuesta => {
+        console.log('respuesta ->', respuesta)
+        this.nombreUsuario = respuesta.nombreUsuario
+})
 }
+}
+
