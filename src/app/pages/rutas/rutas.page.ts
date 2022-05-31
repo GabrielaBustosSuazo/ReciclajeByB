039365808a483fceaa4d22c30208d32b47fc8ede
NeutronCL <jbecofla@gmail.com>
2022-05-31 19:12:37 -0400

    nuevo diseno rutas + organizacion de codigo

--- src/app/pages/rutas/rutas.page.ts
+++ src/app/pages/rutas/rutas.page.ts
@@ -10,21 +10,22 @@ import { FirestoreauthService } from 'src/app/services/firestoreauth.service';
   styleUrls: ['./rutas.page.scss'],
 })
 export class RutasPage implements OnInit {
-  rutas : Rutas [] = []
-  usuario: Usuario [] = []
+  rutas: Rutas[] = [];
+  usuario: Usuario[] = [];
   nombreUsuario: string;
-  constructor(private database: FirestoreService,
-              private alertController: AlertController,
-              private firestoreauth: FirestoreauthService) { 
-    this.firestoreauth.stateUser().subscribe( resp => {
-      if(resp){
-        this.getUserInfo(resp.uid)
-        console.log('esta logeado')
+  constructor(
+    private database: FirestoreService,
+    private alertController: AlertController,
+    private firestoreauth: FirestoreauthService
+  ) {
+    this.firestoreauth.stateUser().subscribe((resp) => {
+      if (resp) {
+        this.getUserInfo(resp.uid);
+        console.log('esta logeado');
+      } else {
+        console.log('no esta logeado');
       }
-      else{
-        console.log('no esta logeado')
-      }
-  })
+    });
   }
 
   ngOnInit() {
@@ -48,37 +49,61 @@ export class RutasPage implements OnInit {
     });
   }
 
-  getUserInfo(uid: string){
-    const path = 'Usuarios'
-        const id = uid;
-        this.database.getUserInfo<Usuario>(path, id).subscribe(respuesta => {
-        console.log('respuesta ->', respuesta)
-        this.nombreUsuario = respuesta.nombreUsuario;
-        console.log(this.nombreUsuario)
-        })
+  getUserInfo(uid: string) {
+    const path = 'Usuarios';
+    const id = uid;
+    this.database.getUserInfo<Usuario>(path, id).subscribe((respuesta) => {
+      console.log('respuesta ->', respuesta);
+      this.nombreUsuario = respuesta.nombreUsuario;
+      console.log(this.nombreUsuario);
+    });
   }
 
   async confirmarRecoleccion() {
     const alert = await this.alertController.create({
       header: 'Confirmar recolección',
       message: '¿Deseas confirmar la recolección?',
-      cssClass: '.yaravi',
       buttons: [
-          {
-            text: 'Denegar',
-            handler: (blah) => {
-              console.log('Confirma Permiso Denegado: yes');
-            }
-          }, {
-            text: 'Permitir',
-            handler: () => {
-              setTimeout(function () {
-                location.reload();
-              }, 100);
-            }
-          }
-        ]
-      });
-      await alert.present();
+        {
+          text: 'Denegar',
+          handler: (blah) => {
+            console.log('Confirma Permiso Denegado: yes');
+          },
+        },
+        {
+          text: 'Permitir',
+          handler: () => {
+            setTimeout(function () {
+              location.reload();
+            }, 100);
+          },
+        },
+      ],
+    });
+    await alert.present();
+  }
+
+  async recoleccionFallida() {
+    const alert = await this.alertController.create({
+      header: 'Confirmar recolección',
+      message: '¿La recolección fue fallida?',
+      buttons: [
+        {
+          text: 'Denegar',
+          handler: (blah) => {
+            console.log('Confirma Permiso Denegado: yes');
+          },
+        },
+        {
+          text: 'Permitir',
+          handler: () => {
+            setTimeout(function () {
+              location.reload();
+            }, 100);
+          },
+        },
+      ],
+    });
+    await alert.present();
   }
 }
