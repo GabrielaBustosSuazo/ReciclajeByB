96ba21b53ff718a5876ec30cdf8cc33c10e94f22
NeutronCL <jbecofla@gmail.com>
2022-06-12 15:21:32 -0400

    diseño menu cliente

--- src/app/pages/seguimiento-evidencias/seguimiento-evidencias.page.ts
+++ src/app/pages/seguimiento-evidencias/seguimiento-evidencias.page.ts
@@ -3,42 +3,43 @@ import { Evidencias } from 'src/app/models/models';
 import { FirestoreService } from 'src/app/services/firestore.service';
 import { formatDate } from '@angular/common';
 
-
 @Component({
   selector: 'app-seguimiento-evidencias',
   templateUrl: './seguimiento-evidencias.page.html',
   styleUrls: ['./seguimiento-evidencias.page.scss'],
 })
 export class SeguimientoEvidenciasPage implements OnInit {
-  
-
-  fechaElegida: string; 
+  fechaElegida: string;
   formatedDate: string;
- 
-  
-  constructor(private database: FirestoreService,
-              ) {
-                
 
- 
-  }
+  constructor(private database: FirestoreService) {}
   evidencias: Evidencias[] = [];
 
   ngOnInit() {
     this.getEvidencias();
-    
   }
 
   SendDataonChange(event: any) {
-      this.formatedDate = formatDate(this.fechaElegida, 'dd/MM/yyyy', 'en');
-      console.log(this.formatedDate)
+    const contenedor = document.querySelector('.mensaje-error');
+
+    const tabla = document.querySelector('.test');
+    if (tabla != null) {
+      contenedor.classList.remove('hidden');
+      console.log(tabla != null, 'se ve');
+    } else {
+      console.log(tabla == null, 'no se ve');
+
+      contenedor.classList.add('hidden');
     }
-  
+    this.formatedDate = formatDate(this.fechaElegida, 'dd/MM/yyyy', 'en');
+    console.log(this.formatedDate);
+  }
 
   getEvidencias() {
     this.database.getCollection<Evidencias>('Evidencias').subscribe((res) => {
       if (res) {
         this.evidencias = res;
+        console.log(this.evidencias);
       }
     });
   }
